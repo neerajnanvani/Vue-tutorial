f579e68ac37810c389ce102e1170a801435cf4f0
neerajnanvani <neeraj.nanvani@vidyamantra.com>
2021-12-13 11:18:07 +0530

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p class="cyan  indigo--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, sapiente. Debitis repudiandae officiis dolorum error blanditiis labore aliquam illo quas eligendi sapiente porro vero nesciunt, praesentium impedit saepe dolor numquam.</p>
+    <p class="display-4 text-capitalize">Massive Display text</p>
+    <v-btn class="blue white--text">Click</v-btn>
+    <v-btn plain color="blue">CLick</v-btn>
+    <v-btn depressed color="blue"> click</v-btn>
+
+    <v-btn plain color="blue">
+      Email
+      <v-icon> mdi-email
+      </v-icon>
+    </v-btn>
+
+    <v-btn class="blue darken-3 cyan--text text--lightn-5">Home
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn>Fingerprint
+      <v-icon>mdi-fingerprint</v-icon>
+    </v-btn>
+    
+    <v-btn>Cell
+      <v-icon> mdi-cellphone</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
+  export default {
+    name: 'Home',
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    components: {
+    
+    },
   }
-}
 </script>
